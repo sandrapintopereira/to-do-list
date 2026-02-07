348e81e991cde719d5c10fe2b9e0a52b1ebf8d43
Sandra Pereira <sandrajesuspintopereira@gmail.com>
2026-02-07 18:16:27 +0000

    eliminar tarefas no botão

--- toDoList.js
+++ toDoList.js
@@ -1,6 +1,6 @@
 const formTarefa = document.getElementById("form-tarefa");
 const inputTarefa = document.getElementById("adicionar-tarefa");
-const listaTarefas = document.getElementById("lista-tarefas");
+const listaTarefas = document.querySelector("#lista-tarefas");
 
 formTarefa.addEventListener("submit", (e) => {
     e.preventDefault(); //evitar reload da página
@@ -23,9 +23,18 @@ formTarefa.addEventListener("submit", (e) => {
   inputTarefa.value = "";
 });
 
-listaTarefas.addEventListener("click", (event) => {
-  if (event.target.classList.contains("check-tarefa")) {
-    const li = event.target.closest("li");
+
+listaTarefas.addEventListener("click", (e) => {
+  if (e.target.classList.contains("check-tarefa")) {
+    const li = e.target.closest("li");
     li.classList.toggle("tarefa-concluida");
+  }
+});
+
+
+listaTarefas.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-remover")) {
+    const li = e.target.closest("li");
+    li.remove();
   }
 });
