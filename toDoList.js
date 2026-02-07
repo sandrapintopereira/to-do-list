7eb39d28099130d6545509dcba8ac5e9b9eb4256
Sandra Pereira <sandrajesuspintopereira@gmail.com>
2026-02-07 17:18:09 +0000

    adicionar tarefa à lista, com preventDefault para não dar reload à página, se só tiver os espaços (faz return), cria o elemento li,o elemento item-lista e adiciona o texto com checkbox e botão de remover (tirei isso da estrutura HTML pois não fazia sentido sem ter a tarefa lá, inputTarefa no final para limpar a caixa de input

--- toDoList.js
+++ toDoList.js
@@ -1,6 +1,24 @@
 const formTarefa = document.getElementById("form-tarefa");
+const inputTarefa = document.getElementById("adicionar-tarefa");
+const listaTarefas = document.getElementById("lista-tarefas");
 
 formTarefa.addEventListener("submit", (e) => {
     e.preventDefault(); //evitar reload da página
-    console.log("Form Submetido");
+    
+    
+    const textoTarefa = inputTarefa.value.trim();
+
+    if (textoTarefa === "") return;
+
+    const li = document.createElement("li");
+    li.classList.add("item-lista");
+
+    li.innerHTML = `
+        <input type="checkbox" class="check-tarefa">
+        <span>${textoTarefa}</span>
+        <button class="btn-remover">X</button>
+    `;
+
+  listaTarefas.appendChild(li);
+  inputTarefa.value = "";
 });
